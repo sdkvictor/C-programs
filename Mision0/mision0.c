2e220fdc400533f193b909287960d950d854413d
root <root@Victor.localdomain>
2020-03-01 19:25:06 -0600

    document and video

--- Mision0/mision0.c
+++ Mision0/mision0.c
@@ -1,18 +1,33 @@
+//
+// Author: Victor Andrés Villarreal Grimaldo
+// ID: A01039863
+// Date: 1/03/2020
+//
+// Create agent structures to store in lists and delete agents from such list
+
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
 #include <stdbool.h>
 
+//Node structure
+// stores a character chain with the mission code or the active code
+// next pointer pointing to the next element in the linked list
 typedef struct Node{ 
     char value[50]; 
     struct Node* next; 
 } Node;
 
+// linked list of nodes with a head (start of the list) and tail (end of the list) pointers
 typedef struct NodeList{ 
     Node* head;
     Node* tail; 
 } NodeList;
 
+//Agent structure node
+// Name and last name of the agent
+// Age of the agent
+// Linked list of actives and list of missions
 typedef struct Agent{ 
    char name[50]; 
    char lastName[50]; 
@@ -22,13 +37,16 @@ typedef struct Agent{
    struct Agent* next;
 } Agent;
 
+//Agents linked list with a head and tail poiting to the first and the last node of the list respectively
 typedef struct{ 
     Agent* head;
     Agent* tail;
 } Agents;
 
+//Linked list of all agents
 Agents agentes;
 
+//Declaration of functions to be documented and defined after the main function
 void displayList(Node* n);
 
 void newAgent(char name[50], char lastName[50], int age);
@@ -48,41 +66,54 @@ void displayAgent(Agent* agent, int count);
 void deleteAgent(char name[50], char lastName[50]);
 
 int main(){
+    //option from the menu
     int op;
+    //name of the agent
     char name[50]; 
+    //last name of the agent
     char lastName[50]; 
+    //Age of the agent
     int age;
+    //Condition of the program being exited or not
     bool end = false;
+    //Initialize the beginning of the agents list pointing to NULL
     agentes.head = NULL;    
 
     while(!end){
+    //Display menu with options
     printf("MENU \n");
-    printf("Ingresa el número de la opción a ejecutar: \n");
-    printf("1. Crear un agente \n");
-    printf("2. Eliminar un agente \n");
-    printf("3. Mostrar agentes \n");
-    printf("4. Salir \n");
+    printf("Enter the number of the action to perform: \n");
+    printf("1. Create new agent \n");
+    printf("2. Delete agent \n");
+    printf("3. Show agents \n");
+    printf("4. Exit \n");
     scanf("%d", &op);
         switch (op){
+            //Create new agent
             case 1:
-                printf("Ingresa el nombre del agente:");
+                printf("Enter the agent's name:");
                 scanf("%s",name);
-                printf("Ingresa el apellido del agente:");
+                printf("Enter the agent's last name:");
                 scanf("%s",lastName);
-                printf("Ingresa la edad del agente:");
+                printf("Enter the agent's age:");
                 scanf("%d", &age);
+                //Create the agent node and add it to the list with the data provided
                 newAgent(name, lastName, age);
                 break;
+            //Delete agent
             case 2:
-                printf("Ingresa el nombre del agente a eliminar:");
+                printf("Enter the name of the agent to delete:");
                 scanf("%s",name);
-                printf("Ingresa el apellido del agente:");
+                printf("Enter the agent's last name:");
                 scanf("%s",lastName);
+                //Delete the specified agent
                 deleteAgent(name, lastName);
                 break;
+            //Display agents
             case 3:
                 displayAgents();
                 break;
+            //Exit the program
             case 4:
                 end = true;
                 break;
@@ -90,7 +121,9 @@ int main(){
     }
 }
 
+//Display the list of missions or actives of an agent
 void displayList(Node* n){
+    //Until reaching the end of the list
     while (n != NULL) { 
         printf("%s", n->value); 
         n = n->next; 
@@ -98,7 +131,15 @@ void displayList(Node* n){
 }
 
 void newAgent(char name[50], char lastName[50], int age){
+    //Node for the new agent
     Agent *agent;
+    //Mission code
+    char mission[50];
+    //Active code
+    char activo[50];
+    //Amount of missions and actives
+    int n;
+    //memory allocation and initialization of nulls
     agent = (struct Agent*)malloc(sizeof(struct Agent));
     agent->activos.head = NULL;
     agent->activos.tail = NULL;
@@ -111,35 +152,34 @@ void newAgent(char name[50], char lastName[50], int age){
     strcpy(agent->lastName,lastName);
 
     //adding missions
-    int n;
-    char mission[50];
-    printf("Ingresa la cantidad de misiones del agente:");
+    printf("Enter the amount of missions of the agent:");
     scanf("%d", &n);
     for(int i=0; i<n; i++){
-        printf("Ingresa el código de la misión %d :", i+1);
+        //input mission code
+        printf("Enter the code of the mission %d :", i+1);
         scanf("%s", mission);
         //validate mission
         while(!validateMission(mission)){
             printf("Error: Please enter a valid mission ID (3 letters followed by 9 whole numbers): \n");
             scanf("%s", mission);
         }
-
+        //Add the mission to the list
         addMission(agent, mission, i);    
     }
     
     //adding actives
-    char activo[50];
-    printf("Ingresa la cantidad de activos del agente:");
+    printf("Enter the amount of actives of the agent:");
+    //input active code
     scanf("%d", &n);
     for(int i=0; i<n; i++){
-        printf("Ingresa el código del activo %d :", i+1);
+        printf("Enter the code of the active %d :", i+1);
         scanf("%s", activo);
         //validate active
         while(!validateActive(activo)){
             printf("Error: Please enter a valid active ID (4 letters followed by 9 whole numbers): \n");
             scanf("%s", activo);
         }
-        
+        //Add the active to the list
         addActive(agent, activo, i);    
     }
 
@@ -152,20 +192,26 @@ void newAgent(char name[50], char lastName[50], int age){
         agentes.tail->next = agent;
         agentes.tail = agent;
     } 
+    //set end of the list pointing to null
     agentes.tail->next = NULL;
 }
 
+//Adds a new mission to the linked list of the specified agent
+//agent - agent owner of the list
+//mission - mission code to be validated and added
+//i - number of element in the list
 void addMission(Agent* agent, char mission[50], int i){
         //set new empty node pointer pointing to NULL
         Node* newMission = NULL;
         newMission = (Node*)malloc(sizeof(Node));
         strcpy(newMission->value, mission);  
         newMission->next = NULL;
-
+        //If its the first element
         if(i==0){
             agent->missions.head = newMission;
             agent->missions.tail = agent->missions.head;
         }
+        //not the first element
         else{
             agent->missions.tail->next = newMission;
             agent->missions.tail = newMission;
@@ -178,11 +224,12 @@ void addActive(Agent* agent, char activo[50], int i){
         newActive = (Node*)malloc(sizeof(Node));
         strcpy(newActive->value, activo);  
         newActive->next = NULL;
-
+        //if its the first element
         if(i==0){
             agent->activos.head = newActive;
             agent->activos.tail = agent->activos.head;
         }
+        //not the first element
         else{
             agent->activos.tail->next = newActive;
             agent->activos.tail = newActive;
@@ -191,17 +238,17 @@ void addActive(Agent* agent, char activo[50], int i){
 
 bool validateMission(char mission[50]){
     bool result;
+    //length is less than 12
     if (strlen(mission) < 12){
         return false;
     }
-
+    //Validate first 3 letters
     for(int i=0; i<3; i++){
-        int errors;
         if(mission[i] < 'A' || (mission[i] > 'Z' && mission[i] < 'a') || mission[i] > 'z'){
             return false;
         } 
     }
-
+    //Validate last 9 digits
     for(int i=3; i<12; i++){
         if(mission[i] < '0' || mission[i]>'9'){
             return false;
@@ -213,17 +260,17 @@ bool validateMission(char mission[50]){
 
 bool validateActive(char activo[50]){
     bool result;
-    if (strlen(activo) < 12){
+    //length is less than 13
+    if (strlen(activo) < 13){
         return false;
     }
-
+    //Validate first 4 letters
     for(int i=0; i<4; i++){
-        int errors;
         if(activo[i] < 'A' || (activo[i] > 'Z' && activo[i] < 'a') || activo[i] > 'z'){
             return false;
         } 
     }
-
+    //Validate last 9 digits
     for(int i=4; i<13; i++){
         if(activo[i] < '0' || activo[i]>'9'){
             return false;
@@ -234,17 +281,25 @@ bool validateActive(char activo[50]){
 }
 
 void displayAgents(){
+    //point to the start of the list
     Agent* currentAgent = agentes.head;
+    //count of the number of agents displayed
     int count = 1;
+    //Until reaching the end of the list
     while(currentAgent!=NULL){
+        //Display the agent's information
         displayAgent(currentAgent, count);
+        //move to the next agent and increase the amount displayed
         currentAgent = currentAgent->next;
         count++;
         printf("\n");
     }
 }
 
+//displays one agent and their data.
+//receives the agent node and the amount of agents displayed so far
 void displayAgent(Agent* agent, int count){
+    //Display name, last name and age
     printf("Agent %d \n", count);
     printf("Name: %s \n", agent->name);
     printf("Last name: %s \n", agent->lastName);
@@ -267,10 +322,15 @@ void displayAgent(Agent* agent, int count){
     }
 }
 
+//delete an agent from the agents list
+//receives the name and alst name of the agent to be deleted
 void deleteAgent(char name[50], char lastName[50]){
+    //Two pointers pointing to the start of the agents list
     Agent* currentAgent = agentes.head;
     Agent* prevAgent = agentes.head;
+    //set found condition to not found yet (false)
     bool found = false;
+    //current position of the node checked
     int count = 1;
     while(currentAgent!=NULL && !found){
         printf("Looking for %s %s \n", name, lastName);       
@@ -278,9 +338,12 @@ void deleteAgent(char name[50], char lastName[50]){
         if(count == 1){
             //found in the first node of the list
             if(strcmp(currentAgent->name, name)==0 && strcmp(currentAgent->lastName, lastName)==0){
+                //Agent was found
                 found = true;
+                //move pointers to skip the deleted node
                 agentes.head = agentes.head->next;
                 currentAgent->next = NULL;
+                //free the memory allocated to the deleted agent
                 free(currentAgent);
                 currentAgent = agentes.head;
                 prevAgent = agentes.head;
@@ -292,22 +355,29 @@ void deleteAgent(char name[50], char lastName[50]){
         else{
             if(strcmp(currentAgent->name, name)==0 && strcmp(currentAgent->lastName, lastName)==0){
                 found = true;
+                //move pointers to skip the deleted node
                 prevAgent->next = currentAgent->next;
                 currentAgent->next = NULL;
+                //free the memory allocated to the deleted agent
                 free(currentAgent);
+                //Move to the next node
                 currentAgent = prevAgent->next;
                 
             }
             else{
+                //Move to the next node, and move both pointers forward
                 prevAgent =prevAgent->next;
                 currentAgent = currentAgent->next;
             }
         }
+        //increase the nodes checked
         count++;
     }
+    //Agent was not found
     if(!found){
         printf("Error: Agent was not found \n");
     }
+    //Agent was found
     else{
         printf("Agent deleted successfully! \n");
     }
